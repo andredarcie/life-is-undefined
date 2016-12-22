4aad5f08f39fec7fd26db8537862b9d263226f5a
André N. Darcie <andrendarcie@gmail.com>
2016-12-22 15:33:00 -0200

    Removing the old update method

--- js/main.js
+++ js/main.js
@@ -1,59 +1,12 @@
-var $gameTitle, $imageBox, $message, $listGroup,
-    $choiceZero, $choiceOne, stateLength;
-
 $(document).ready(function() {
   start();
 });
 
 function start(){
-
-    stateLength = state.length;
-
-    $gameTitle = $('#game-title');
-    $imageBox = $('#image-box');
-    $message = $('#message');
-    $listGroup = $('#list-group');
-    $choiceZero = $('#choice-zero');
-    $choiceOne = $('#choice-one');
-
-    updateFixed(0);
-
+    update(0);
 }
 
 function update(currentStateId){
-
-  var p, currentState, choiceZero, choiceOne,
-      messageLength, html;
-
-  if(currentStateId < stateLength){
-    currentState = state[currentStateId];
-    $gameTitle.text(currentState.title);
-    $imageBox.attr('src','img/' + currentState.image + '.png');
-    //$message.find('p').text('"' + currentState.message + '"');
-
-    $message.empty();
-
-    messagesLength = currentState.messages.length;
-    html = '';
-    for (var i = 0; i < messagesLength; i++){
-      html += '<p>"' + currentState.messages[i] + '"</p>';
-    }
-    $message.html(html);
-
-    choiceZero = currentState.choices.zero;
-    $choiceZero.text(choiceZero.message);
-    $choiceZero.click(function() { update(choiceZero.nextstate); });
-
-    choiceOne = currentState.choices.one;
-    $choiceOne.text(choiceOne.message);
-    $choiceOne.click(function() { update(choiceOne.nextstate); });
-  } else {
-    console.log('Error - Invalid State: ' + currentStateId);
-  }
-
-}
-
-function updateFixed(currentStateId){
   var i, html, imageUrl, messagesLength, choicesLength,
   messagesHtml = '', choicesHtml = '';
 
@@ -78,7 +31,7 @@ function updateFixed(currentStateId){
   choicesLength = currentState.choices.length;
   for (i = 0; i < choicesLength; i++){
     choicesHtml += '<li class="list-group-item"' +
-    ' onclick="updateFixed(' + currentState.choices[i].nextstate + ')">' +
+    ' onclick="update(' + currentState.choices[i].nextstate + ')">' +
     currentState.choices[i].message + '</li>';
   }
   html += '<div id="choices"><ul id="list-group">' +

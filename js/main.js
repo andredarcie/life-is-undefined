619fa8be225d053f8b4b50789cae92a0e2f87bd2
André N. Darcie <andrendarcie@gmail.com>
2016-12-22 15:29:47 -0200

    Creating a new update method to improve performance

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ function start(){
     $choiceZero = $('#choice-zero');
     $choiceOne = $('#choice-one');
 
-    update(0);
+    updateFixed(0);
 
 }
 
@@ -50,5 +50,41 @@ function update(currentStateId){
   } else {
     console.log('Error - Invalid State: ' + currentStateId);
   }
+
+}
+
+function updateFixed(currentStateId){
+  var i, html, imageUrl, messagesLength, choicesLength,
+  messagesHtml = '', choicesHtml = '';
+
+  currentState = state[currentStateId];
+
+  /* game title */
+  html = '<h1 id="game-title">' + currentState.title + '</h1>';
+
+  /* image */
+  imageUrl = 'img/' +  currentState.image + '.png';
+  html += '<img id="image-box" src="' + imageUrl + '"' +
+          'alt="Test Image" height="700" width="500">';
+
+  /* messages */
+  messagesLength = currentState.messages.length;
+  for (i = 0; i < messagesLength; i++){
+    messagesHtml += '<p>"' + currentState.messages[i] + '"</p>';
+  }
+  html += '<div id="message">' + messagesHtml + '</div>';
+
+  /* choices */
+  choicesLength = currentState.choices.length;
+  for (i = 0; i < choicesLength; i++){
+    choicesHtml += '<li class="list-group-item"' +
+    ' onclick="updateFixed(' + currentState.choices[i].nextstate + ')">' +
+    currentState.choices[i].message + '</li>';
+  }
+  html += '<div id="choices"><ul id="list-group">' +
+           choicesHtml + '</ul></div>';
+
+  /* DOM */
+  $('#container').html(html);
 
 }
--- js/states.js
+++ js/states.js
@@ -1,31 +1,17 @@
 var state = [
     {
-      title: 'Mysterious Game [1/4]',
+      title: 'Mysterious Game 1',
       image: 'test-image',
       messages: ['Observo na tela do computador o código que acabo de criar...',
-                 'AAAAAAAAAAAA'],
-      choices: {zero: {message: 'Ficar um tempo olhando a tela', nextstate: 1},
-                one: {message: "Ir embora", nextstate: 1}}
+                 '...'],
+      choices: [{message: 'Ficar um tempo olhando a tela', nextstate: 1},
+                {message: "Ir embora", nextstate: 1}]
     },
     {
-      title: 'Mysterious Game [2/4]',
+      title: 'Mysterious Game 2',
       image: 'eu',
       messages: ['Esse sou eu...'],
-      choices: {zero: {message: 'Chorar de pena', nextstate: 2},
-                one: {message: "Fechar jogo", nextstate: 1}}
-    },
-    {
-      title: 'Mysterious Game [3/4]',
-      image: 'eu',
-      messages: ['Esse sou eu...'],
-      choices: {zero: {message: 'Chorar de pena', nextstate: 3},
-                one: {message: "Fechar jogo", nextstate: 1}}
-    },
-    {
-      title: 'Mysterious Game [4/4]',
-      image: 'eu',
-      messages: ['Esse sou eu...'],
-      choices: {zero: {message: 'Chorar de pena', nextstate: 0},
-                one: {message: "Fechar jogo", nextstate: 1}}
+      choices: [{message: 'Chorar de pena', nextstate: 0},
+                {message: "Fechar jogo", nextstate: 0}]
     }
 ];
